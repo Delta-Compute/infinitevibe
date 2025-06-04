90bbe75b844770bb9d7428874a47bb1fb9e1a0d0
toilaluan <toilaluan@github.com>
2025-06-04 02:50:15 +0000

    add filterAI

--- tensorflix/services/platform_tracker/utils.py
+++ tensorflix/services/platform_tracker/utils.py
@@ -1,24 +1,42 @@
 import requests
+import requests
+import json
+import cv2
+from io import BytesIO
+
 def filter_AI_video(video_url: str, start_time: int, end_time: int, fps: int = 24) -> bool:
-    api_endpoint = "https://api.bitmind.ai/oracle/v1/34/detect-video"
-    payload = {
-        "video": video_url,
-        "startTime": start_time,
-        "endTime": end_time,
-        "fps": fps,
-        "rich": True
+    params = {
+        'models': 'genai',
+        'api_user': '1000474889',
+        'api_secret': 'g6MKC4ybq9PMxA2ESii9KDGYHwhjtkfX'
     }
-    headers = {
-        "Authorization": "Bearer oracle-f3459c05-7e7e-4335-add1-247fef4d0c68:17eb465d"
-    }
-    response = requests.post(
-        api_endpoint,
-        headers=headers,
-        json=payload
-    )
-    is_AI = response.json()["isAI"]
-    print("is AI:", is_AI)
-    return is_AI
+
+    response = requests.get(video_url)
+    with open("request_video.mp4", 'wb') as f:
+        f.write(response.content)
+
+    video_capture = cv2.VideoCapture("request_video.mp4")
+    ret, frame = video_capture.read()
+
+    if ret:
+        _, buffer = cv2.imencode('.jpg', frame)
+        image_file = BytesIO(buffer.tobytes())
+
+        files = {'media': ('frame.jpg', image_file, 'image/jpeg')}
+        
+        try:
+            r = requests.post('https://api.sightengine.com/1.0/check.json', 
+                            files=files, 
+                            data=params)
+            
+            output = r.json()
+            ai_generated_score = output["type"]["ai_generated"]
+            return ai_generated_score > 0.5
+        except Exception as e:
+            print(f"Error making API request: {e}")
+    else:
+        print("Failed to read frame from video")
+    return False
 
 def filter_caption_video(video_caption: str) -> bool:
     list_of_valid_captions = [
@@ -31,9 +49,11 @@ def filter_caption_video(video_caption: str) -> bool:
     return True
 
 if __name__ == "__main__":
-    filter_AI_video(
+    is_AI = filter_AI_video(
         video_url="https://pub-a55bd0dbae3c4afd86bd066961ab7d1e.r2.dev/test_10secs.mov",
         start_time=1,
         end_time=5,
         fps=24
     )
+    print(is_AI)
+
